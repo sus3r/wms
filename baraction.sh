9f08186fd462983966fa6a25b87c5ad6e571668e
soo-go <110265206+soo-go@users.noreply.github.com>
2023-01-29 11:44:10 -0300

    upgrade

--- baraction.sh
+++ baraction.sh
@@ -18,7 +18,7 @@ total="$(free | grep Mem: | awk '{print $2}')"
 human="$(free -h | grep Mem: | awk '{print $3}' | sed s/i//g)"
 
 ram="$(( 200 * $used/$total - 100 * $used/$total ))% ($human)"
-			echo "+@fg=7;[RAM:$ram]"
+			echo "+@fg=0;[+@fg=7;RAM:$ram+@fg=0;]"
 }
 ##############################
 #	    CPU
@@ -31,7 +31,7 @@ cpu() {
 	        read cpu a b c idle rest < /proc/stat
 		  total=$((a+b+c+idle))
 		    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-		    		echo "+@fg=8;[CPU:$cpu%]"
+		    		echo "+@fg=0;[+@fg=2;CPU:$cpu%+@fg=0;]"
 	    } 
 ##############################
 #   VOLUME
@@ -39,7 +39,7 @@ cpu() {
 
 vol() {
     VOL="$(amixer -D pipewire get Master | grep Left: | sed 's/[][]//g' | awk '{print $5}')"
-    		echo "+@fg=5;[VOL:$VOL]"
+    		echo "+@fg=0;[+@fg=5;VOL:$VOL+@fg=0;]"
 	}
 ##############################
 #	    Packages
@@ -56,42 +56,48 @@ vol() {
 #upgrades(){
 	#upgrades="$(aptitude search '~U' | wc -l)"
 	#echo "$upgrades"
-#}
-##############################
-#	    NETWORK
-##############################
+#} 
+ 
+ #NETWORK
 network() {
 wire="$(ip a | grep "eth0\|enp" | grep inet | wc -l)"
 wifi="$(ip a | grep wlan | grep inet | wc -l)"
 
 if [ $wire = 1 ]; then
-    echo "+@fg=1;[WIFI: ETH]"
+    echo "[+@fg=4;WIFI:ETH+@fg=0;]"
 elif [ $wifi = 1 ]; then
-    echo "+@fg=1;[WIFI: UP ]"
+    echo "[+@fg=1;WIFI:UP+@fg=0;]"
 else
-    echo "+@fg=3;[WIFI:DOWN]"
+    echo "[+@fg=3;WIFI:DOWN+@fg=0;]"
 fi
-
 }
 
 ipaddress() {
     address="$(ip a | grep .255 | grep -v wlp | cut -d ' ' -f6 | sed 's/\/24//')"
-    echo "+@fg=0;[IP:$address]"
+    echo "+@fg=0;[+@fg=8;IP:$address+@fg=0;]"
 }
 
 vpnconnection() {
     state="$(ip a | grep tun0 | grep inet | wc -l)"
 
 if [ $state = 1 ]; then
-    echo "+@fg=1;[VPN: UP ]"
+    echo "[+@fg=1;VPN:UP+@fg=0;]"
 else
-    echo "+@fg=3;[VPN:DOWN]"
+    echo "[+@fg=3;VPN:DOWN+@fg=0;]"
+	#echo "+@fg=3;[VPN:DOWN]"
 fi
 }
 # BATTERY
 bat() {
+status="$(cat /sys/class/power_supply/BAT1/status)"
 battery="$(cat /sys/class/power_supply/BAT1/capacity)"
-		echo "[$battery%]"
+	if [$status = 'Charging'];then	
+		echo "+@fg=1;[+$battery%]"
+	elif[$status = 'Discharging']
+		echo "+@fg=0;[$battery%]"
+	else 
+		echo "+@fg=0;[$battery%]"
+	fi
 }
 
 layout_info(){
@@ -103,7 +109,7 @@ layout_info(){
 $(cpu)\
 $(mem)\
 $(vol)\
-$(ipaddress)$(vpnconnection)\
+ $(ipaddress)$(vpnconnection)\
 $(network)\
 "
         sleep 1
