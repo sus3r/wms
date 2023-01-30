28c6bf42602d24ce2d51b73dacabef6680a2c5ae
soo-go <110265206+soo-go@users.noreply.github.com>
2023-01-29 21:04:59 -0300

    upgrade

--- baraction.sh
+++ baraction.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 hostname="${HOSTNAME:-${hostname:-$(hostname)}}"
 
-##############################
-#	    DISK
-##############################
-
+#DISK
 #hdd() { free="$(df -h /home | grep /dev | awk '{print $3}' | sed 's/G/Gb/')" perc="$(df -h /home | grep /dev/ | awk '{print $5}')"
 #     echo "$perc  ($free)"
 #   }
-##############################
-#	    RAM
-##############################
-
+#RAM
 mem() {
 used="$(free | grep Mem: | awk '{print $3}')"
 total="$(free | grep Mem: | awk '{print $2}')"
@@ -20,45 +14,33 @@ human="$(free -h | grep Mem: | awk '{print $3}' | sed s/i//g)"
 ram="$(( 200 * $used/$total - 100 * $used/$total ))% ($human)"
 			echo "+@fg=0;[+@fg=7;RAM:$ram+@fg=0;]"
 }
-##############################
-#	    CPU
-##############################
-
+#CPU
 cpu() {
-	  read cpu a b c previdle rest < /proc/stat
-	    prevtotal=$((a+b+c+previdle))
-	      sleep 0.5
-	        read cpu a b c idle rest < /proc/stat
-		  total=$((a+b+c+idle))
-		    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-		    		echo "+@fg=0;[+@fg=2;CPU:$cpu%+@fg=0;]"
-	    } 
-##############################
-#   VOLUME
-##############################
+	 read cpu a b c previdle rest < /proc/stat
+	   prevtotal=$((a+b+c+previdle))
+	     sleep 0.5
+	       read cpu a b c idle rest < /proc/stat
+		 total=$((a+b+c+idle))
+		   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+		   		echo "+@fg=0;[+@fg=2;CPU:$cpu%+@fg=0;]"
+	   } 
 
+#VOLUME
 vol() {
     VOL="$(amixer -D pipewire get Master | grep Left: | sed 's/[][]//g' | awk '{print $5}')"
-    		echo "+@fg=0;[+@fg=5;VOL:$VOL+@fg=0;]"
-	}
-##############################
-#	    Packages
-##############################
-
+	echo "+@fg=0;[+@fg=5;V:$VOL+@fg=0;]"
+}
+#Packages
 #pkgs() {
 	#pkgs="$(dpkg -l | grep -c ^i)"
 	#echo "$pkgs"
 #}
-##############################
-#	    UPGRADES
-##############################
-
+#UPGRADES
 #upgrades(){
 	#upgrades="$(aptitude search '~U' | wc -l)"
 	#echo "$upgrades"
 #} 
- 
- #NETWORK
+#NETWORK
 network() {
 wire="$(ip a | grep "eth0\|enp" | grep inet | wc -l)"
 wifi="$(ip a | grep wlan | grep inet | wc -l)"
@@ -87,31 +69,24 @@ else
 	#echo "+@fg=3;[VPN:DOWN]"
 fi
 }
-# BATTERY
+#BATTERY
 bat() {
 status="$(cat /sys/class/power_supply/BAT1/status)"
 battery="$(cat /sys/class/power_supply/BAT1/capacity)"
-	if [$status = 'Charging'];then	
-		echo "+@fg=1;[+$battery%]"
-	elif[$status = 'Discharging']
-		echo "+@fg=0;[$battery%]"
-	else 
-		echo "+@fg=0;[$battery%]"
-	fi
+		echo "+@fg=0;[+@fg=6;B:$battery%+@fg=0;]"
 }
 
 layout_info(){
    xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print "RU"; else print "EN";}'
 }
 	while :; do
-	      echo "$(layout_info)\
+	     echo "$(layout_info)\
  $(bat)\
+$(vol)\
 $(cpu)\
 $(mem)\
-$(vol)\
  $(ipaddress)$(vpnconnection)\
 $(network)\
 "
-        sleep 1
+        sleep 3
 		done
-
